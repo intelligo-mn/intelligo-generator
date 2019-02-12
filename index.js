71539e27660601e21801fbc30f95c0ff8cce6bbb
Turtuvshin Byambaa <toroo.byamba@gmail.com>
2019-02-13 00:24:52 +0900

    cli create

--- index.js
+++ index.js
@@ -1,62 +1,281 @@
 #!/usr/bin/env node
-const fs = require('fs')
 
-const program = require('commander')
-const childProcess = require('child_process')
-const port = process.env.port || 3000
-const pjson = require('./package.json')
+var ejs = require('ejs')
+var fs = require('fs')
+var minimatch = require('minimatch')
+var mkdirp = require('mkdirp')
+var path = require('path')
+var program = require('commander')
+var readline = require('readline')
+var sortedObject = require('sorted-object')
+var util = require('util')
 
-function startCommand(command, callback) {
-  const child = childProcess.exec(command, callback)
-  child.stdout.pipe(process.stdout)
-  child.stderr.pipe(process.stderr)
-  return child
-}
+var MODE_0666 = parseInt('0666', 8)
+var MODE_0755 = parseInt('0755', 8)
+var TEMPLATE_DIR = path.join(__dirname, '..', 'templates')
+var VERSION = require('../package').version
 
-function init() {
+var _exit = process.exit
 
-  console.log('Creating new bot...')
+// Re-assign process.exit because of commander
+// TODO: Switch to a different command framework
+process.exit = exit
 
-  if (!fs.existsSync("intelligo-bot")){
-    fs.mkdirSync("intelligo-bot");
+// CLI
+
+around(program, 'optionMissingArgument', function (fn, args) {
+  program.outputHelp()
+  fn.apply(this, args)
+  return { args: [], unknown: [] }
+})
+
+before(program, 'outputHelp', function () {
+  // track if help was shown for unknown option
+  this._helpShown = true
+})
+
+before(program, 'unknownOption', function () {
+  // allow unknown options if help was shown, to prevent trailing error
+  this._allowUnknownOption = this._helpShown
+
+  // show help if not yet shown
+  if (!this._helpShown) {
+    program.outputHelp()
   }
-  process.chdir('/intelligo-bot');
-  fs.readdir('templates/',function(err,files){
-      if(err) throw err;
-      files.forEach(function(file){
-        console.log('Copying '+file)
-        fs.writeFile(flie, data, (err) => {
-          if (err) throw err
-    
-          console.log('Copied '+file)
-        })
-      });
+})
 
-      console.log('Installing Dependencies...')
+program
+  .name('intelligo')
+  .version(VERSION, '    --version')
+  .usage('[dir]')
+  .parse(process.argv)
 
-      startCommand('npm install')
-  });
+if (!exit.exited) {
+  main()
 }
 
-function startServer() {
-  return startCommand('node --use_strict --harmony .',(error, stdout, stderr) => process.exit(1))
+/**
+ * Install an around function; AOP.
+ */
+
+function around (obj, method, fn) {
+  var old = obj[method]
+
+  obj[method] = function () {
+    var args = new Array(arguments.length)
+    for (var i = 0; i < args.length; i++) args[i] = arguments[i]
+    return fn.call(this, old, args)
+  }
 }
 
-program
-  .version(pjson.version)
+/**
+ * Install a before function; AOP.
+ */
 
-program
-  .command('init')
-  .action(() => init())
+function before (obj, method, fn) {
+  var old = obj[method]
 
-program
-  .command('start')
-  .action(() => {
-    startServer()
-    console.log(`Server is running on http://localhost: ${port}`)
+  obj[method] = function () {
+    fn.call(this)
+    old.apply(this, arguments)
+  }
+}
+
+/**
+ * Prompt for confirmation on STDOUT/STDIN
+ */
+
+function confirm (msg, callback) {
+  var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
   })
 
-program
-  .action((cmd, env) => program.outputHelp())
+  rl.question(msg, function (input) {
+    rl.close()
+    callback(/^y|yes|ok|true$/i.test(input))
+  })
+}
 
-program.parse(process.argv)
+/**
+ * Copy file from template directory.
+ */
+
+function copyTemplate (from, to) {
+  write(to, fs.readFileSync(path.join(TEMPLATE_DIR, from), 'utf-8'))
+}
+
+/**
+ * Create application at the given directory.
+ *
+ * @param {string} name
+ * @param {string} dir
+ */
+
+function createApplication (name, dir) {
+  console.log()
+
+  // Package
+  var pkg = {
+    name: name,
+    version: '0.0.0',
+    private: true,
+    scripts: {
+      start: 'node index.js'
+    },
+    dependencies: {
+      'config': '~3.0.1',
+      'express': '~4.16.1',
+      'intelligo': '^0.8.7'
+    }
+  }
+
+  // JavaScript
+  var app = loadTemplate('index.js')
+
+  if (dir !== '.') {
+    mkdir(dir, '.')
+  }
+
+  mkdir(dir, 'config')
+
+  copyTemplateMulti('config/default.json', dir + '/routes', '*.js')
+  copyTemplate('config/default.json', path.join(dir, 'config/default.json'))
+  copyTemplate('.gitignore', path.join(dir, '.gitignore'))
+
+  // write files
+  write(path.join(dir, 'index.js'), app.render())
+  write(path.join(dir, 'package.json'), JSON.stringify(pkg, null, 2) + '\n')
+
+  var prompt = launchedFromCmd() ? '>' : '$'
+
+  if (dir !== '.') {
+    console.log()
+    console.log('   change directory:')
+    console.log('     %s cd %s', prompt, dir)
+  }
+
+  console.log()
+  console.log('   install dependencies:')
+  console.log('     %s npm install', prompt)
+  console.log()
+  console.log('   run the app:')
+
+  if (launchedFromCmd()) {
+    console.log('     %s SET DEBUG=%s:* & npm start', prompt, name)
+  } else {
+    console.log('     %s DEBUG=%s:* npm start', prompt, name)
+  }
+
+  console.log()
+}
+
+/**
+ * Create an app name from a directory path, fitting npm naming requirements.
+ *
+ * @param {String} pathName
+ */
+
+function createAppName (pathName) {
+  return path.basename(pathName)
+    .replace(/[^A-Za-z0-9.-]+/g, '-')
+    .replace(/^[-_.]+|-+$/g, '')
+    .toLowerCase()
+}
+
+/**
+ * Check if the given directory `dir` is empty.
+ *
+ * @param {String} dir
+ * @param {Function} fn
+ */
+
+function emptyDirectory (dir, fn) {
+  fs.readdir(dir, function (err, files) {
+    if (err && err.code !== 'ENOENT') throw err
+    fn(!files || !files.length)
+  })
+}
+
+/**
+ * Main program.
+ */
+
+function main () {
+  // Path
+  var destinationPath = program.args.shift() || '.'
+
+  // App name
+  var appName = createAppName(path.resolve(destinationPath)) || 'hello-world'
+
+  // Generate application
+  emptyDirectory(destinationPath, function (empty) {
+    if (empty || program.force) {
+      createApplication(appName, destinationPath)
+    } else {
+      confirm('destination is not empty, continue? [y/N] ', function (ok) {
+        if (ok) {
+          process.stdin.destroy()
+          createApplication(appName, destinationPath)
+        } else {
+          console.error('aborting')
+          exit(1)
+        }
+      })
+    }
+  })
+}
+
+/**
+ * Make the given dir relative to base.
+ *
+ * @param {string} base
+ * @param {string} dir
+ */
+
+function mkdir (base, dir) {
+  var loc = path.join(base, dir)
+
+  console.log('   \x1b[36mcreate\x1b[0m : ' + loc + path.sep)
+  mkdirp.sync(loc, MODE_0755)
+}
+
+/**
+ * Generate a callback function for commander to warn about renamed option.
+ *
+ * @param {String} originalName
+ * @param {String} newName
+ */
+
+function renamedOption (originalName, newName) {
+  return function (val) {
+    warning(util.format("option `%s' has been renamed to `%s'", originalName, newName))
+    return val
+  }
+}
+
+/**
+ * Display a warning similar to how errors are displayed by commander.
+ *
+ * @param {String} message
+ */
+
+function warning (message) {
+  console.error()
+  message.split('\n').forEach(function (line) {
+    console.error('  warning: %s', line)
+  })
+  console.error()
+}
+
+/**
+ * echo str > file.
+ *
+ * @param {String} file
+ * @param {String} str
+ */
+
+function write (file, str, mode) {
+  fs.writeFileSync(file, str, { mode: mode || MODE_0666 })
+  console.log('   \x1b[36mcreate\x1b[0m : ' + file)
+}
